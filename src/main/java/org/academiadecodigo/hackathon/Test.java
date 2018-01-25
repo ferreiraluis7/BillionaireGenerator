a7551c83c266f1b1fc37fc04b4126e3003c61668
Luís Ferreira <lfgf.90@gmail.com>
2018-01-25 20:56:44 -0100

    minor changes

--- src/main/java/org/academiadecodigo/hackathon/Test.java
+++ src/main/java/org/academiadecodigo/hackathon/Test.java
@@ -2,6 +2,11 @@ package org.academiadecodigo.hackathon;
 
 import javafx.stage.Stage;
 import org.academiadecodigo.hackathon.controller.LoginController;
+import org.academiadecodigo.hackathon.model.User;
+import org.academiadecodigo.hackathon.persistence.AppUserDao;
+import org.academiadecodigo.hackathon.persistence.UserDao;
+import org.academiadecodigo.hackathon.service.AppUserService;
+import org.academiadecodigo.hackathon.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,15 +19,27 @@ public class Test extends javafx.application.Application {
     }
 
     @Override
+    public void init() throws Exception {
+
+    }
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/spring/spring-config.xml");
-        Navigation navigation = applicationContext.getBean("navigation", Navigation.class);
+       /* Navigation navigation = applicationContext.getBean("navigation", Navigation.class);
 
         navigation.setStage(primaryStage);
 
         navigation.loadScreen(LoginController.class.getSimpleName());
 
         primaryStage.setTitle("Billionaire Generator");
-        primaryStage.show();
+        primaryStage.show();*/
+
+        User user = new User("soraia", "sss", "soraia@gmail.com");
+        User user1 = new User();
+        UserService service = (UserService) applicationContext.getBean("userService");
+        service.addUser(user);
+        service.addUser(user1);
+
     }
 }
