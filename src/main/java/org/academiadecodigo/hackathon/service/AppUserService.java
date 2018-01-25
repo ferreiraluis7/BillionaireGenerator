92eaa3466344a46c5384bec87d62268b997402a7
Luís Ferreira <lfgf.90@gmail.com>
2018-01-25 16:54:22 -0100

    implemented app user service

---
+++ src/main/java/org/academiadecodigo/hackathon/service/AppUserService.java
@@ -0,0 +1,26 @@
+package org.academiadecodigo.hackathon.service;
+
+public class AppUserService implements UserService {
+
+    private UserDao dao;
+
+    public void addUser(User user) {
+        dao.saveOrUpdate(user);
+    }
+
+    public void removeUser(User user) {
+        dao.delete(user);
+    }
+
+    public boolean authenticate(String username, String email) {
+        return findbyName(username)!= null && findbyEmail(email) != null;
+    }
+
+    public User findbyName(String username) {
+        dao.findByName(username);
+    }
+
+    public User findbyEmail(String email) {
+        dao.findByEmail(email);
+    }
+}
