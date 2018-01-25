f4898664b072464239086688d27ace85728121db
João Silva <jls.kurogmail.com>
2018-01-25 16:40:12 -0100

    User and Wallet models

---
+++ src/main/java/org/academiadecodigo/hackathon/models/User.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.hackathon.models;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    private Wallet wallet;
+
+}
---
+++ src/main/java/org/academiadecodigo/hackathon/models/Wallet.java
@@ -0,0 +1,9 @@
+package org.academiadecodigo.hackathon.models;
+
+public class Wallet {
+
+    private int id;
+    private double cAmount;
+    private double dAmount;
+
+}
