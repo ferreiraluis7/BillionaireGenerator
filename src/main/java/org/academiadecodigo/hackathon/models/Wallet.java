b4ea441d9c9fbcef4e9b0978bf6f5a8994305936
João Silva <jls.kurogmail.com>
2018-01-25 16:46:40 -0100

    User and Wallet models updated. Currency model added.

--- src/main/java/org/academiadecodigo/hackathon/models/Wallet.java
+++ src/main/java/org/academiadecodigo/hackathon/models/Wallet.java
@@ -6,4 +6,27 @@ public class Wallet {
     private double cAmount;
     private double dAmount;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getcAmount() {
+        return cAmount;
+    }
+
+    public void setcAmount(double cAmount) {
+        this.cAmount = cAmount;
+    }
+
+    public double getdAmount() {
+        return dAmount;
+    }
+
+    public void setdAmount(double dAmount) {
+        this.dAmount = dAmount;
+    }
 }
